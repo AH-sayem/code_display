cb6db976265e1059d67c852d27be69dff8f1689e
Anamul Haque Sayem <88246699+AH-sayem@users.noreply.github.com>
2023-04-27 15:50:36 +0600

    Add files via upload

---
+++ display_code.cpp
@@ -0,0 +1,27 @@
+#line 1 "display_code.ino"
+#include<LiquidCrystal.h>
+
+#include "Arduino.h"
+void setup();
+void loop();
+#line 3
+LiquidCrystal lcd(13,12,11,10,9,8);
+
+void setup() {
+  
+                                             
+  
+  lcd.begin(16,2);
+  lcd.print("hellow world");
+  
+}
+
+void loop() {
+ 
+                                                    
+  lcd.setCursor(0,1);
+  lcd.print("sbs online");
+  delay(1000);
+  lcd.clear();
+}
+
---
+++ display_code.ino
@@ -0,0 +1,21 @@
+#include<LiquidCrystal.h>
+
+LiquidCrystal lcd(13,12,11,10,9,8);
+
+void setup() {
+  
+  // put your setup code here , to run once ;
+  
+  lcd.begin(16,2);
+  lcd.print("hellow world");
+  
+}
+
+void loop() {
+ 
+  // put your main code here , to run respectively ;
+  lcd.setCursor(0,1);
+  lcd.print("sbs online");
+  delay(1000);
+  lcd.clear();
+}
